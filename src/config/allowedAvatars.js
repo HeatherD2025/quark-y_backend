dacbfebbb06fa05b9b756f344a030f93abde0d23
heatherd2025 <heatherdeliso@gmail.com>
2025-08-21 09:27:33 -0400

    avatar work

--- src/config/allowedAvatars.js
+++ src/config/allowedAvatars.js
@@ -1,10 +1,10 @@
 export const allowedAvatarIds = [
-    'avatar1.jpeg',
-    'avatar2.jpeg',
-    'avatar3.jpeg',
-    'avatar4.jpeg',
-    'avatar5.jpeg',
-    'avatar6.jpeg',
-    'avatar7.jpeg',
-    'avatar8.jpeg',
+    'avatar1.png',
+    'avatar2.png',
+    'avatar3.png',
+    'avatar4.png',
+    'avatar5.png',
+    'avatar6.png',
+    'avatar7.png',
+    'avatar8.png',
 ];
