eb6c2d79b081fbc1fe5e8c739ccb1f47b4c205ae
heatherd2025 <heatherdeliso@gmail.com>
2025-09-22 14:24:15 -0400

    comment and article work

--- src/app.js
+++ src/app.js
@@ -24,22 +24,36 @@ app.use(morgan("dev"));
 
 app.use("/api/articles", articleRouter);
 
-// // API Routes
+// API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/comments", commentRoutes);
 
-// // Health Check
+// 
+app.get('/api/articles/:articleUrl', async (req, res) => {
+  const { articleUrl } = req.params;
+
+  const article = await prisma.article.findUnique({
+    where: { articleUrl: decodeURIComponent(articleUrl) },
+    include: { comments: true }
+  });
+
+  if (!article) return res.status(404).json({ message: 'Article not found'});
+
+  res.json(article);
+})
+
+// Health Check
 app.get("/health", (req, res) => {
   res.json({ status: "ok" });
 });
 
-// // 404 for unknown API routes
+// 404 for unknown API routes
 app.use(/.*/, (req, res) => {
   res.status(404).json({ error: "Route not found" });
 });
 
-// // Generic error handler
+// Generic error handler
 app.use((err, req, res, next) => {
   console.error("Unhandled error:", err.stack);
   res
